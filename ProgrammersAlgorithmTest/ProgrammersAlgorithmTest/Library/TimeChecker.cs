ab8e4e4ede38fff4eefaf56d7597eaa8c9219b38
daehyun kang <soejdtodxn@gmail.com>
2021-02-19 15:12:15 +0900

    add timeChecker and Linq test
    add timeChecker and Linq test

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/Library/TimeChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProgrammersAlgorithmTest.Library
+{
+    public static class TimeChecker
+    {
+        public static TimeSpan GetRunningTime(Action action, int repeat)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < repeat; i++)
+                action.Invoke();
+
+            stopWatch.Stop();
+
+            return stopWatch.Elapsed;
+        }
+    }
+}
---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/Tests/Linq.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgrammersAlgorithmTest.Library;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SolutionForAllTest
+{
+    [TestClass]
+    public class Linq
+    {
+        [TestMethod]
+        public void CountingSpecificCharUseCount_MoreSlowThenLoop_UseStringBinary()
+        {
+            var linqRecord = TimeChecker.GetRunningTime(() =>
+            {
+                var randomBinary = Convert.ToString(new Random().Next(), 2);
+                
+                randomBinary.Count(d => d == '1');
+            }, 1000000);
+
+            var loopRecord = TimeChecker.GetRunningTime(() =>
+            {
+                var randomBinary = Convert.ToString(new Random().Next(), 2);
+
+                var count = 0;
+
+                foreach (var item in randomBinary)
+                {
+                    if (item == '1')
+                        count++;
+                }
+            }, 1000000);
+
+            Assert.IsTrue(linqRecord > loopRecord);
+            Debug.WriteLine($"LinqRecord vs LoopRecord");
+            Debug.WriteLine($"{linqRecord} vs {loopRecord}");
+        }
+    }
+}
