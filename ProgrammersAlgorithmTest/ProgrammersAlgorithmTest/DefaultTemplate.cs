cbf7b40cf44fa438b8b548c2225e21564aaf22c2
daehyun kang <soejdtodxn@gmail.com>
2021-02-19 15:11:25 +0900

    add default template
    add default template

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/DefaultTemplate.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammersAlgorithmTest
+{
+    [TestClass]
+    public class DefaultTemplate
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
