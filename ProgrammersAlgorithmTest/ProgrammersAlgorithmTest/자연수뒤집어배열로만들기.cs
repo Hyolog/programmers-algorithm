d1d58919591bd90317ab63309e33de3dbd703f39
Hyolo <soejdtodxn@gmail.com>
2021-03-08 13:29:29 +0900

    자연수뒤집어배열로만들기
    자연수뒤집어배열로만들기

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/자연수뒤집어배열로만들기.cs
@@ -0,0 +1,23 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12932
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammersAlgorithmTest
+{
+    [TestClass]
+    public class 자연수뒤집어배열로만들기
+    {
+        [TestMethod]
+        public void Test()
+        {
+            CollectionAssert.AreEqual(solution(12345), new int[] { 5, 4, 3, 2, 1 });
+        }
+
+        public int[] solution(long n)
+        {
+            return n.ToString().Reverse().Select(d => int.Parse(d.ToString())).ToArray();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 * [짝수와홀수](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/짝수와홀수.cs)
 * [콜라츠추측](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/콜라츠추측.cs)
 * [서울에서김서방찾기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/서울에서김서방찾기.cs)
+* [자연수뒤집어배열로만들기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/자연수뒤집어배열로만들기.cs)
 
 ## LV2
 * [주식가격](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/주식가격.cs)
