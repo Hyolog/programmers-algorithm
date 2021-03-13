92c82e36d9bc928c896d012955a5ad53c3298233
daehyun kang <soejdtodxn@gmail.com>
2021-03-13 18:54:11 +0900

    멀리뛰기
    멀리뛰기

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/멀리뛰기.cs
@@ -0,0 +1,33 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/12914
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ProgrammersAlgorithmTest
+{
+    [TestClass]
+    public class 멀리뛰기
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(solution(4), 5);
+            Assert.AreEqual(solution(3), 3);
+        }
+
+        public long solution(int n)
+        {
+            var result = new List<long>() { 0, 1, 2 };
+
+            if (n < 3)
+                return result[n];
+            
+            for (var i = 3; i <= n; i++)
+            {
+                result.Add((result[i - 1] + result[i - 2]) % 1234567);
+            }
+
+            return result[n];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -35,6 +35,7 @@
 ## LV3
 * [베스트앨범](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/베스트앨범.cs)
 * [디스크컨트롤러](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/디스크컨트롤러.cs)
+* [멀리뛰기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/멀리뛰기.cs)
 
 ## LV4
 * [쿠키구입](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/쿠키구입.cs)
