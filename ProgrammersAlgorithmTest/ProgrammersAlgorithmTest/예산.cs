23cd95d2728d5f202c67230d6d03819b5bc6609c
Hyolo <soejdtodxn@gmail.com>
2021-03-30 11:16:18 +0900

    예산
    예산

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/예산.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammersAlgorithmTest
+{
+    /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/12982"/>
+    [TestClass]
+    public class 예산
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(solution(new int[] { 1, 3, 2, 5, 4 }, 9), 3);
+        }
+
+        public int solution(int[] d, int budget)
+        {
+            var result = 0;
+            var departments = new List<int>(d).OrderBy(i => i);
+
+            foreach (var department in departments)
+            {
+                budget -= department;
+
+                if (budget < 0)
+                    break;
+
+                result++;
+            }
+
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,7 @@
 * [모의고사](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/모의고사.cs)
 * [평균구하기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/평균구하기.cs)
 * [문자열내림차순으로배치하기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/문자열내림차순으로배치하기.cs)
+* [예산](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/예산.cs)
 
 ## LV2
 * [주식가격](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/주식가격.cs)
