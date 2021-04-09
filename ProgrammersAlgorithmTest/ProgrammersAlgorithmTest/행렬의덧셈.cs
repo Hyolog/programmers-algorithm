64cbe488e02490ec9657f62ffe35c3db50747a4e
daehyun kang <soejdtodxn@gmail.com>
2021-04-10 01:47:40 +0900

    update 행렬의덧셈
    update 행렬의덧셈

--- ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/행렬의덧셈.cs
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/행렬의덧셈.cs
@@ -14,9 +14,9 @@ namespace ProgrammersAlgorithmTest
 
         public int[,] solution(int[,] arr1, int[,] arr2)
         {
-            for (var i = 0; i < arr1.GetLength(1); i++)
+            for (var i = 0; i < arr1.GetLength(0); i++)
             {
-                for (var j = 0; j < arr1.GetLength(0); j++)
+                for (var j = 0; j < arr1.GetLength(1); j++)
                 {
                     arr1[i, j] += arr2[i, j];
                 }
