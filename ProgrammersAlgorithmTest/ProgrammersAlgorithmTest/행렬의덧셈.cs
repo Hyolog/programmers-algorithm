b138a95c9b0f1f3821ea87a7ac0f54ca8170669d
daehyun kang <soejdtodxn@gmail.com>
2021-04-10 01:46:12 +0900

    행렬의덧셈
    행렬의덧셈

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/행렬의덧셈.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammersAlgorithmTest
+{
+    /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/12950"/>
+    [TestClass]
+    public class 행렬의덧셈
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+
+        public int[,] solution(int[,] arr1, int[,] arr2)
+        {
+            for (var i = 0; i < arr1.GetLength(1); i++)
+            {
+                for (var j = 0; j < arr1.GetLength(0); j++)
+                {
+                    arr1[i, j] += arr2[i, j];
+                }
+            }
+
+            return arr1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -20,6 +20,7 @@
 * [두정수사이의합](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/두정수사이의합.cs)
 * [문자열내마음대로정렬하기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/문자열내마음대로정렬하기.cs)
 * [약수의합](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/약수의합.cs)
+* [행렬의덧셈](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/행렬의덧셈.cs)
 
 ## LV2
 * [주식가격](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/주식가격.cs)
