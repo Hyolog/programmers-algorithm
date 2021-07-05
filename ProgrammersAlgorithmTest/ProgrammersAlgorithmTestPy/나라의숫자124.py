673a0f405b68c621d082b68f727a0e6150bd9a10
daehyun kang <soejdtodxn@gmail.com>
2021-07-05 18:23:58 +0900

    파이썬 프로젝트 unittest 제거

--- ProgrammersAlgorithmTest/ProgrammersAlgorithmTestPy/나라의숫자124.py
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTestPy/나라의숫자124.py
@@ -1,8 +1,8 @@
 #https://programmers.co.kr/learn/courses/30/lessons/12899?language=python3
 
-def solution2(n):
+def solution(n):
     if n <= 3:
-        return '124'[n-1]
+        return '124'[n - 1]
     else:
-        q, r = divmod(n-1, 3)
-        return solution2(q) + '124'[r]
+        q, r = divmod(n - 1, 3)
+        return solution(q) + '124'[r]
