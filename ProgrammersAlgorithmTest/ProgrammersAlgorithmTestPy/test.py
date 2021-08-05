d7e0908b3decabec140777d9429bffb399164ec7
daehyun kang <soejdtodxn@gmail.com>
2021-08-06 01:07:16 +0900

    Add python test file
    add test.py that include test format using unittest

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTestPy/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+def solution(arr):
+    return true
+
+    
+class TestStringMethods(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(solution(), true)
+
+
+if __name__ == '__main__':
+    unittest.main()
