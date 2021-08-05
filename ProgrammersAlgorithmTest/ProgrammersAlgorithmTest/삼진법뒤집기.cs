33678b62f746d755f00237f0299b6f2ca560c59b
daehyun kang <soejdtodxn@gmail.com>
2021-08-06 02:18:37 +0900

    삼진법뒤집기 update
    삼진법뒤집기 update

--- ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/삼진법뒤집기.cs
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/삼진법뒤집기.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
+using System.Text;
 
 namespace ProgrammersAlgorithmTest
 {
     /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/68935"/>
-    /// TODO : Lv1 치고 어렵다.
+    /// DONE : Lv1 치고 어렵다.
     [TestClass]
     public class 삼진법뒤집기
     {
@@ -16,39 +18,28 @@ namespace ProgrammersAlgorithmTest
 
         public int solution(int n)
         {
-            string reverseTernary = "";
+            var ternaryString = new StringBuilder();
 
-            while (n > 0)
+            var portion = n;
+
+            while (portion > 0)
             {
-                reverseTernary += n % 3;
+                var rest = portion % 3;
+                portion = portion / 3;
 
-                if (n / 3 == 0)
-                    break;
-
-                n /= 3;
+                ternaryString.Append(rest);
             }
 
-            int result = 0;
+            var ternary = new string(ternaryString.ToString().Reverse().ToArray());
 
-            for (var i = 0; i < reverseTernary.Length; i++)
+            var result = 0;
+
+            for (int i = 0; i < ternary.Length; i++)
             {
-                result += int.Parse(reverseTernary[i].ToString()) * (int)Math.Pow(3, reverseTernary.Length - 1 - i);
+                result += (int)char.GetNumericValue(ternary[i]) * (int)Math.Pow(3, i);
             }
 
             return result;
-        }
-
-        // 좋은 풀이 기록
-        public int solution2(int n)
-        {
-            int answer = 0;
-            while (n > 0)
-            {
-                answer *= 3;
-                answer += n % 3;
-                n /= 3;
-            }
-            return answer;
         }
     }
 }
