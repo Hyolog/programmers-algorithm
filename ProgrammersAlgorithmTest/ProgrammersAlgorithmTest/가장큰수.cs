222d59b68e21d73a27d1e898ba5c49be681c2b4b
daehyun kang <soejdtodxn@gmail.com>
2021-08-08 19:01:20 +0900

    가장큰수 update
    다시 풀어보기

--- ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/가장큰수.cs
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/가장큰수.cs
@@ -1,19 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
 
 namespace ProgrammersAlgorithmTest
 {
     /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/42746"/>
-    /// TODO : 참고한 풀이. 다시한번 보면 좋을듯
+    /// DONE : 참고한 풀이. 다시한번 보면 좋을듯
     [TestClass]
     public class 가장큰수
     {
         [TestMethod]
         public void Test()
         {
-            //Assert.AreEqual(solution(new int[] { 0, 0, 1000 }), "100000");
-            //Assert.AreEqual(solution(new int[] { 6 }), "6");
-            //Assert.AreEqual(solution(new int[] { 0 }), "0");
             Assert.AreEqual(solution(new int[] { 0, 0 }), "0");
             Assert.AreEqual(solution(new int[] { 6, 10, 2 }), "6210");
             Assert.AreEqual(solution(new int[] { 3, 30, 34, 5, 9 }), "9534330");
@@ -22,10 +20,11 @@ namespace ProgrammersAlgorithmTest
         /// <param name="numbers">1~100,000</param>
         public string solution(int[] numbers)
         {
-            // DP 느낌으로
-            Array.Sort(numbers, (x, y) => string.Compare(y.ToString() + x.ToString(), x.ToString() + y.ToString()));
+            var nums = numbers.Select(d => d.ToString()).ToArray();
 
-            return numbers[0] == 0 ? "0" : string.Join("", numbers);
+            Array.Sort(nums, (x, y) => string.Compare(y + x, x + y));
+
+            return nums[0] == "0" ? "0" : string.Join("", nums);
         }
     }
 }
