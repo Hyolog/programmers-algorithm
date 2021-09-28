da9a3f9c4b9c7b6038bceaeac45557ee9dccc21e
dhkang <soejdtodxn@naver.com>
2021-09-28 23:28:45 +0900

    [LV1]없는숫자더하기

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/없는숫자더하기.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace ProgrammersAlgorithmTest
+{
+    /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/86051"/>
+    [TestClass]
+    public class 없는숫자더하기
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+
+        public int solution(int[] numbers)
+        {
+            return 45 - numbers.Sum();
+        }
+    }
+}
