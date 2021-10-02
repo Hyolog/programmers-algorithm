ea0474b00faf65c97eb41036c7ccf3180c1da8f0
dhkang <soejdtodxn@naver.com>
2021-10-02 13:06:34 +0900

    [LV2]입실퇴실

---
+++ ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/입실퇴실.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgrammersAlgorithmTest
+{
+    /// <see cref="https://programmers.co.kr/learn/courses/30/lessons/86048"/>
+    [TestClass]
+    public class 입실퇴실
+    {
+        [TestMethod]
+        public void Test()
+        {
+            CollectionAssert.AreEqual(solution(new int[] { 1, 3, 2 }, new int[] { 1, 2, 3 }), new int[] { 0, 1, 1 });
+            CollectionAssert.AreEqual(solution(new int[] { 1, 4, 2, 3 }, new int[] { 2, 1, 3, 4 }), new int[] { 2, 2, 1, 3 });
+            CollectionAssert.AreEqual(solution(new int[] { 3, 2, 1 }, new int[] { 2, 1, 3 }), new int[] { 1, 1, 2 });
+            CollectionAssert.AreEqual(solution(new int[] { 3, 2, 1 }, new int[] { 1, 3, 2 }), new int[] { 2, 2, 2 });
+            CollectionAssert.AreEqual(solution(new int[] { 1, 4, 2, 3 }, new int[] { 2, 1, 4, 3 }), new int[] {2, 2, 0, 2 });
+        }
+
+        public int[] solution(int[] enter, int[] leave)
+        {
+            var answerDic = new Dictionary<int, HashSet<int>>(); 
+            var answer = new int[enter.Length];
+            var intersect = new List<int>();
+
+            for (int i = 0; i < leave.Length; i++)
+            { 
+                var index = Array.IndexOf(enter, leave[i]); 
+                var count = index; 
+                var lastCount = leave.Length - i - 1; 
+                
+                if (count <= 0 || lastCount <= 0) 
+                    continue; 
+                
+                intersect = enter.Take(count).Intersect(leave.TakeLast(lastCount)).ToList(); 
+                intersect.Add(leave[i]); 
+                
+                foreach (var item in intersect) 
+                { 
+                    if (!answerDic.ContainsKey(item - 1)) 
+                        answerDic.Add(item - 1, new HashSet<int>()); 
+                    
+                    foreach (var node in intersect) 
+                    { 
+                        if (item == node) 
+                            continue; 
+                        
+                        answerDic[item - 1].Add(node); 
+                    } 
+                } 
+                
+                enter[index] = 0; 
+            }
+
+            foreach (var e in answerDic) 
+                answer[e.Key] += e.Value.Count; 
+            
+            return answer;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -82,6 +82,7 @@
 * [게임맵최단거리](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/게임맵최단거리.cs)
 * [거리두기확인하기](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/거리두기확인하기.cs)
 * [모음사전](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/모음사전.cs)
+* [입실퇴실](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/입실퇴실.cs)
 
 ## LV3
 * [베스트앨범](ProgrammersAlgorithmTest/ProgrammersAlgorithmTest/베스트앨범.cs)
